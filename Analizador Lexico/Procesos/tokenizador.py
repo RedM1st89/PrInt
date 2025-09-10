25b0952516cb721b98e9be2e56060aef03f82f66
Enzo Gianola <punchisxl2@gmail.com>
2025-09-09 17:27:20 -0700

    Funcional hasta strings de una palabra, falta cambio para poder procesar strings con espacios

--- Analizador Lexico/Procesos/tokenizador.py
+++ Analizador Lexico/Procesos/tokenizador.py
@@ -3,14 +3,25 @@ from Procesos.transiciones import num
 
 def tokenizacion(lexi, texto):
     resultado = []
-    cont_string = 0
     palabras = texto.split()
     for palabra in palabras:
         estado = lexi.estado_inicial
         palanca_string = 0
+        letra_pos = 0
         for letra in palabra:
             trans = lexi.estados.get(estado, {})
-            if letra in trans:
+            if letra in ('"'):
+                if letra_pos == 0:
+                    estado = trans['"']
+                    palanca_string = 1
+                if letra_pos == len(palabra)-1:
+                    estado = trans['"']
+            elif palanca_string == 1:
+                if letra in (" "):
+                    continue
+                else:
+                    estado = trans['string']
+            elif letra in trans:
                 estado = trans[letra]
             elif letra >= 'a' and letra <= 'z':
                 estado = trans['minuschar']
@@ -20,7 +31,8 @@ def tokenizacion(lexi, texto):
                 estado = trans['sim']
             else:
                 raise ValueError(f"no es posible tokenizar '{palabra}'")
-        cont_string += 1
+            letra_pos += 1
+            print(letra_pos)
         if estado in lexi.estados_finales:
             token = llaves.get(estado)
             if token:
